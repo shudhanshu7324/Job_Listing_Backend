475c07b07e1e3da15f298e804e01cd7238e3ed08
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-11-11 19:45:23 +0530

    add home route

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const env = require('dotenv')
 env.config();
 const PORT = process.env.PORT || 3000;
 
+app.get('/',(req,res) => {
+    res.send("Hello World")
+})
+
 app.listen(PORT,()=>{
     console.log("Server is running on PORT ",PORT)
 })
